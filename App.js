7c4498f59c8c110c1db54ffd8cddea97d5839f86
erik <em222iv@student.lnu.se>
2017-11-23 17:54:15 +0100

    map, timecalc and android styling

--- App.js
+++ App.js
@@ -10,15 +10,25 @@ import {
     ListView,
     Dimensions,
     ImageBackground,
-    TouchableWithoutFeedback
+    TouchableWithoutFeedback,
+    StyleSheet
 } from 'react-native';
 import { TabNavigator, DrawerNavigator, StackNavigator, Header } from 'react-navigation';
+
 const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 const ds2 = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 const ds3 = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 
-const {width} = Dimensions.get('window')
+
+const {width,height} = Dimensions.get('window')
 const thumbnailSize = width/3 - 2;
+
+import MapView from 'react-native-maps';
+const ASPECT_RATIO = width / height;
+const LATITUDE = 37.78825;
+const LONGITUDE = -122.4324;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 const Ad = ({ad, navigation}) => {
   return (
@@ -32,7 +42,7 @@ const Ad = ({ad, navigation}) => {
             <Text style={{marginHorizontal:10,color:'white', fontSize:12, fontWeight:'bold'}}>{ad.title}</Text> 
             <View style={{flexDirection:'row',justifyContent:'space-between',alignItems:'flex-end', backgroundColor:'transparent'}}>
               <Text style={{margin:10,color:'white', fontSize:20, fontWeight:'bold'}}>{ad.pay}</Text>
-              <Text style={{margin:10,color:'white', fontSize:20, fontWeight:'bold'}}>1d</Text>
+              <Text style={{margin:10,color:'white', fontSize:20, fontWeight:'bold'}}>{timeAgo(getTimestampFromItemId(ad._id))}</Text>
             </View>
         </View>
         </View>
@@ -148,16 +158,17 @@ class SimpleScreen3 extends Component {
 }
 
 class AdView extends Component {
-  componentDidMount() {}
   render() {
     const{ad} = this.props.navigation.state.params;
+    let uri = ad.thumbnail.substring(0, ad.thumbnail.length - 2)
     return (
+      <ScrollView>
       <View style={{ flex: 1 }}>
-        <ImageBackground source={{uri: ad.thumbnail}} style={{width:width, height:width}}>
+        <ImageBackground source={{uri: uri}} style={{width:width, height:width}}>
           <View style={{flex:1,flexDirection:'column', justifyContent:'flex-end'}}>
           <View style={{flexDirection:'row', justifyContent:'space-between', backgroundColor:'transparent'}}>
               <Text style={{fontSize:40, color:'white', fontWeight:'bold', margin:10}}>{ad.pay}KR</Text>
-              <Text style={{fontSize:40, color:'white', fontWeight:'bold', margin:10}}>{ad.pay}MIL</Text>
+              <Text style={{fontSize:40, color:'white', fontWeight:'bold', margin:10}}>{timeAgo(getTimestampFromItemId(ad._id))}</Text>
             </View>
           </View>
         </ImageBackground>
@@ -165,7 +176,33 @@ class AdView extends Component {
           <Text style={{fontSize:30, color:'black', fontWeight:'bold', margin:10}}>{ad.title}</Text>
            <Text style={{fontSize:12, color:'black', fontWeight:'bold', margin:10}}>{ad.desc}</Text>
         </View>
+        <View style ={{
+           height: 200,
+           width: width,
+        }}>
+        <MapView
+          style={{  ...StyleSheet.absoluteFillObject,}}
+          region={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+        >
+          <MapView.Marker
+              title="This is a title"
+              description="This is a description"
+              coordinate={{
+                latitude: LATITUDE,
+                longitude: LONGITUDE,
+                latitudeDelta: LATITUDE_DELTA,
+                longitudeDelta: LONGITUDE_DELTA
+              }}
+            />
+        </MapView>
+        </View>
       </View>
+      </ScrollView>
     );
   }
 }
@@ -200,7 +237,9 @@ const MainGrid = TabNavigator({
   tabBarOptions: {
     style: {
       backgroundColor: "#51A7F9",
-      borderTopWidth: 0
+      borderTopWidth: 0,
+      borderBottomWidth: 0,
+      elevation:0
     },
     labelStyle: {
       color: "#FFF"
@@ -217,7 +256,8 @@ const MainStack = StackNavigator({
       headerStyle: {
         backgroundColor: "#51A7F9",
         borderBottomWidth: 0,
-        borderTopWidth: 0
+        borderTopWidth: 0,
+        elevation:0
       },
       headerLeft: <Button title="Info" onPress={() => navigation.navigate('DrawerOpen')}/>
     })
@@ -270,5 +310,29 @@ const ModalStack = StackNavigator({
   headerMode:'none',
 });
 
+const getTimestampFromItemId = inAsHex => {
+  return parseInt(inAsHex.slice(0, 11), 16);
+};
+const timeAgo = (date) => {
+  var seconds = Math.floor((new Date() - date) / 1000);
+  var interval = Math.floor(seconds / 31536000);
+  interval = Math.floor(seconds / 420000);
+  if (interval >= 1) {
+    return interval + 'w';
+  }
+  interval = Math.floor(seconds / 86400);
+  if (interval >= 1) {
+    return interval + 'd';
+  }
+  interval = Math.floor(seconds / 3600);
+  if (interval >= 1) {
+    return interval + 'h';
+  }
+  interval = Math.floor(seconds / 60);
+  if (interval >= 1) {
+    return interval + 'm';
+  }
+  return Math.floor(seconds) + 's';
+}
 
 export default ModalStack;
